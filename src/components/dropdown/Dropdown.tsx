bfd326bb6a6f8a949f92b8e794c0bc33f9024bc9
SimonDMC <simondmcplayer@gmail.com>
2025-06-16 16:04:55 +0200

    Add animation to profile dropdown

--- src/components/dropdown/Dropdown.tsx
+++ src/components/dropdown/Dropdown.tsx
@@ -1,5 +1,16 @@
 import "./Dropdown.css";
+import { motion } from "framer-motion";
 
-export default function Dropdown(props: { open: boolean; children?: React.ReactNode }) {
-    return <div className={(props.open ? "open" : "closed") + " dropdown"}>{props.children}</div>;
+export default function Dropdown(props: { children?: React.ReactNode }) {
+    return (
+        <motion.div
+            initial={{ opacity: 0, x: 15 }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0, x: 15 }}
+            transition={{ duration: 0.2 }}
+            className="dropdown"
+        >
+            {props.children}
+        </motion.div>
+    );
 }
--- src/components/profile-icon/ProfileIcon.tsx
+++ src/components/profile-icon/ProfileIcon.tsx
@@ -23,6 +23,7 @@ import { logout } from "../../util/auth";
 import { type Options, setCodeword, setupBioAuth, switch2fa } from "../../util/options";
 import { showKeyHash } from "../../util/encryption";
 import { useNavigate } from "@tanstack/react-router";
+import { AnimatePresence } from "framer-motion";
 
 export default function ProfileIcon() {
     const [profileDropdownOpen, setProfileDropdownOpen] = useState(false);
@@ -53,63 +54,75 @@ export default function ProfileIcon() {
             <a onClick={() => setProfileDropdownOpen(!profileDropdownOpen)}>
                 <FontAwesomeIcon icon={faUser} />
             </a>
-            <Dropdown open={profileDropdownOpen}>
-                <DropdownText label={username.current} />
-                <DropdownSeparator />
-                <DropdownHeading label="Actions" />
-                <DropdownItem
-                    label="Generate Key"
-                    description="Generate a new key to encrypt and decrypt all of your entries with when communicating with the remote database"
-                    onClick={generateKey}
-                />
-                <DropdownItem label="Upload Key" description="Upload a key from a KEY file" onClick={uploadKey} />
-                <DropdownItem label="Download Key" description="Export the locally saved key into a KEY file" onClick={downloadKey} />
-                <DropdownItem label="Export" description="Export all entries saved locally into a JSON file" onClick={download} />
-                <DropdownItem
-                    label="Import"
-                    description="Import entries from a JSON file - DELETES ALL CURRENTLY SAVED ENTRIES FROM THE DATABASE!"
-                    onClick={upload}
-                />
-                <DropdownItem label="Change Password" onClick={changePassword} />
-                <DropdownSeparator />
-                <DropdownHeading label="2FA" />
-                <DropdownItem
-                    label="Switch 2FA"
-                    description="Switch between no 2fa, codeword and biometric auth"
-                    onClick={() => switch2fa(options, setOptions)}
-                />
-                {/* only render "set codeword" or "setup bioauth" if the corresponding settings are selected */}
-                {options["2fa_method"] == 1 && <DropdownItem label="Set Codeword" onClick={() => setCodeword(options, setOptions)} />}
-                {options["2fa_method"] == 2 && <DropdownItem label="Setup Biometry" onClick={() => setupBioAuth(options, setOptions)} />}
-                <DropdownSeparator />
-                <DropdownHeading label="Debug" />
-                <DropdownItem label="Force Reload" description="Delete local page cache and reload" onClick={forceReload} />
-                <DropdownItem
-                    label="Show Key Hash"
-                    description="Show a hash of your encryption key for debug purposes"
-                    onClick={showKeyHash}
-                />
-                <DropdownItem
-                    label="Wipe Local DB"
-                    description="Delete all locally saved entries (resyncs with the database on page reload)"
-                    onClick={wipeLocalDatabase}
-                />
-                {/* <DropdownItem
-                    label="Invoke Update Popup"
-                    onClick={() => {
-                        updateDownload.dispatchEvent(
-                            new CustomEvent("done", {
-                                detail: {
-                                    version: "0.0.0",
-                                    changelogs: ["Example update log"],
-                                },
-                            })
-                        );
-                    }}
-                /> */}
-                <DropdownSeparator />
-                <DropdownItem label="Log Out" onClick={() => logout(navigate)} />
-            </Dropdown>
+            <AnimatePresence>
+                {profileDropdownOpen && (
+                    <Dropdown>
+                        <DropdownText label={username.current} />
+                        <DropdownSeparator />
+                        <DropdownHeading label="Actions" />
+                        <DropdownItem
+                            label="Generate Key"
+                            description="Generate a new key to encrypt and decrypt all of your entries with when communicating with the remote database"
+                            onClick={generateKey}
+                        />
+                        <DropdownItem label="Upload Key" description="Upload a key from a KEY file" onClick={uploadKey} />
+                        <DropdownItem
+                            label="Download Key"
+                            description="Export the locally saved key into a KEY file"
+                            onClick={downloadKey}
+                        />
+                        <DropdownItem label="Export" description="Export all entries saved locally into a JSON file" onClick={download} />
+                        <DropdownItem
+                            label="Import"
+                            description="Import entries from a JSON file - DELETES ALL CURRENTLY SAVED ENTRIES FROM THE DATABASE!"
+                            onClick={upload}
+                        />
+                        <DropdownItem label="Change Password" onClick={changePassword} />
+                        <DropdownSeparator />
+                        <DropdownHeading label="2FA" />
+                        <DropdownItem
+                            label="Switch 2FA"
+                            description="Switch between no 2fa, codeword and biometric auth"
+                            onClick={() => switch2fa(options, setOptions)}
+                        />
+                        {/* only render "set codeword" or "setup bioauth" if the corresponding settings are selected */}
+                        {options["2fa_method"] == 1 && (
+                            <DropdownItem label="Set Codeword" onClick={() => setCodeword(options, setOptions)} />
+                        )}
+                        {options["2fa_method"] == 2 && (
+                            <DropdownItem label="Setup Biometry" onClick={() => setupBioAuth(options, setOptions)} />
+                        )}
+                        <DropdownSeparator />
+                        <DropdownHeading label="Debug" />
+                        <DropdownItem label="Force Reload" description="Delete local page cache and reload" onClick={forceReload} />
+                        <DropdownItem
+                            label="Show Key Hash"
+                            description="Show a hash of your encryption key for debug purposes"
+                            onClick={showKeyHash}
+                        />
+                        <DropdownItem
+                            label="Wipe Local DB"
+                            description="Delete all locally saved entries (resyncs with the database on page reload)"
+                            onClick={wipeLocalDatabase}
+                        />
+                        {/* <DropdownItem
+                            label="Invoke Update Popup"
+                            onClick={() => {
+                                updateDownload.dispatchEvent(
+                                    new CustomEvent("done", {
+                                        detail: {
+                                            version: "0.0.0",
+                                            changelogs: ["Example update log"],
+                                        },
+                                    })
+                                );
+                            }}
+                        /> */}
+                        <DropdownSeparator />
+                        <DropdownItem label="Log Out" onClick={() => logout(navigate)} />
+                    </Dropdown>
+                )}
+            </AnimatePresence>
         </div>
     );
 }
